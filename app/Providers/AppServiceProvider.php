014707e78ab4909fd2e6e46d0823f8787d2c1f0f
yohan vishvajith <yohanvishvajith@gmail.com>
2024-03-21 01:00:58 +0530

    laravel structure
    first laravel structure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +17,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+    
+public function boot()
+{
+    Schema::defaultStringLength(191);
+}
 }
